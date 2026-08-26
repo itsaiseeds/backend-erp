e9dca496c562356b15609ff0db75e923330b165e
LowLevelLore <mihirom9876@gmail.com>
2026-08-26 23:08:32 +0530

    Add whitenoise for static files - auto-collected on every deploy

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ MIGRATION_MODULES = {app.split(".")[-1]: None for app in INSTALLED_APPS}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
